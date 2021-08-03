af7b6daefc3ccc4b9765d721c45f3d68372fd5ee
kimself <ksy7678@gmail.com>
2021-08-03 21:10:57 +0900

    출처 url 추가

--- stringS.py
+++ stringS.py
@@ -1,3 +1,6 @@
+# 문자열 내 p와 y의 개수
+# https://programmers.co.kr/learn/courses/30/lessons/12916
+
 def solution(s):
     # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
     print('Hello Python')
--- watermelon.py
+++ watermelon.py
@@ -1,3 +1,7 @@
+# 수박수박수
+# https://programmers.co.kr/learn/courses/30/lessons/12922
+
+
 def solution(n):
     answer = ''
     return ('수박'*n)[:n]
