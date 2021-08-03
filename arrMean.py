2d9e49b85ee8d91ce3aeae1363ba02fb8173d61c
kimself <ksy7678@gmail.com>
2021-08-03 22:46:39 +0900

    평균 구하기

---
+++ arrMean.py
@@ -0,0 +1,14 @@
+# 평균 구하기
+# https://programmers.co.kr/learn/courses/30/lessons/12944?language=python3
+
+def solution(arr):
+    answer = 0
+    answer = sum(arr)/len(arr)
+    return answer
+
+arr = [1,2,3,4]
+# arr = [5, 5]
+solution(arr)
+
+# 다른 사람 코드
+# 내게 제일 간단하당! 신난당!
