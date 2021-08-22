ecd305a700a92da29a3781ea51bca67832e5eea1
kimself <ksy7678@gmail.com>
2021-08-22 20:52:47 +0900

    연산자 문풀 추가 0822

--- JunGol/03_operator.py
+++ JunGol/03_operator.py
@@ -73,3 +73,33 @@ num1AfterAdd += 1
 num2 -= 1 # 전치 감소 연산자
 multipNum = num1*num2
 print(num1AfterAdd, num2, num1*num2)
+
+
+
+print()
+print('='*100)
+'''
+두 개의 정수를 입력받아서, 
+첫 번째 줄에는 두 정수의 값이 같으면 1 아니면 0을 출력하고, 
+두 번째 줄에는 같지 않으면 1 같으면 0을 출력하는 프로그램을 작성하시오.
+(JAVA는 1이면 true, 0이면 false를 출력한다.)
+'''
+'''
+입력 예
+5 5
+
+출력 예
+1
+0
+'''
+num1, num2 = map(int, input().split())
+
+if num1 == num2:
+    print(1)
+else:
+    print(0)
+
+if num1 != num2:
+    print(1)
+else:
+    print(0)
