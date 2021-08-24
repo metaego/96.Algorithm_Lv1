c7e76a1d68239402346ce98c39658789cb14a37c
kimself <ksy7678@gmail.com>
2021-08-24 17:40:06 +0900

    정올 문제업로드 0824

--- JunGol/03_operator.py
+++ JunGol/03_operator.py
@@ -122,8 +122,21 @@ print('='*100)
 4 >= 5 --- 0
 4 <= 5 --- 1
 '''
-num1, num2 = map(int, input().split())
-print(num1, '>', num2, '---', int(num1 > num2))
-print(num1, '<', num2, '---', int(num1 < num2))
-print(num1, '>=', num2, '---', int(num1 >= num2))
-print(num1, '<=', num2, '---', int(num1 <= num2))
+# num1, num2 = map(int, input().split())
+# print(num1, '>', num2, '---', int(num1 > num2))
+# print(num1, '<', num2, '---', int(num1 < num2))
+# print(num1, '>=', num2, '---', int(num1 >= num2))
+# print(num1, '<=', num2, '---', int(num1 <= num2))
+
+print()
+print('='*100)
+'''
+2개의 정수를 입력 받아서 논리곱과 논리합의 결과를 출력하는 프로그램을 작성하시오.
+(수가 0 이 아닌 경우 참으로, 0 인 경우 거짓으로 간주합니다.)
+
+[JAVA]
+2개의 정수를 입력 받아서 0이 아니면 참(true), 0이면 거짓(false)으로 처리하고
+두 값의 논리곱과 논리합의 결과를 출력하는 프로그램을 작성하시오.
+
+​hint : 정수 a를 입력받은 후 boolean c = (a != 0);을 실행하면 c에 a의 논리값이 저장된다.
+'''
