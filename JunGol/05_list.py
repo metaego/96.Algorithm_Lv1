dfbbd44e5784a8f096f22f1a16059621a85b3844
kimself <ksy7678@gmail.com>
2021-09-02 02:14:29 +0900

    파이썬 리스트 문풀2 0902

--- JunGol/05_list.py
+++ JunGol/05_list.py
@@ -6,5 +6,29 @@
 출력 예
 ['Salad', 'Pizza', 'Chicken', 'Soup']
 '''
-lst = ['Salad', 'Pizza', 'Chicken', 'Soup']
-print(lst)
+# lst = ['Salad', 'Pizza', 'Chicken', 'Soup']
+# print(lst)
+
+print()
+print('='*100)
+'''
+'Python', 'is', 'exciting' 세 문자열로 이루어진 리스트를 만들고, 
+다음과 같이 출력하는 프로그램을 작성하라.​
+'''
+'''
+출력 예
+Python is exciting
+'''
+lst = ['Python', 'is', 'exciting']
+print(' '.join(lst))
+
+# 추가로 문자열을 리스트로 변환하는 방법은
+# str변수명.split(" ")
+
+# , 로 문자열의 내용을 구분하고 싶을 때
+# str변수명.split(",")  
+
+# 문자열을 글자 단위로 분리하여 리스트로 변환 
+# data = list("ABC")
+# print(data)
+# print(*data) 
