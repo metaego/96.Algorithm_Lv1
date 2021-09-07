9e759622794575a9d727645ae12fb8a9aa8626b0
kimself <ksy7678@gmail.com>
2021-09-08 03:21:36 +0900

    파이썬 리스트 문풀6 0908

--- JunGol/05_list.py
+++ JunGol/05_list.py
@@ -83,7 +83,35 @@ Eagle
 E
 g
 '''
-txt = input()
-print(txt[0])
-lst = "".join(txt)
-print(lst[2])
+# txt = input()
+# print(txt[0])
+# lst = "".join(txt)
+# print(lst[2])
+
+
+print()
+print('='*100)
+'''
+원소 6개짜리 리스트를 입력받아 생성한 후, 
+인덱스를 입력 받아 
+입력받은 인덱스부터 맨 마지막까지의 부분 리스트를 출력하는 
+프로그램을 작성하라.
+'''
+'''
+입력 예
+Element? Seoul
+Element? New York City	
+Element? Tokyo	
+Element? Beijing	
+Element? London	
+Element? Paris	
+Index? 3
+
+출력 예
+['Beijing', 'London', 'Paris']
+'''
+lst = []
+for i in range(6):
+    lst.append(str(input('Element? ')))
+idx_number = int(input('Index? '))
+print(lst[idx_number:])
